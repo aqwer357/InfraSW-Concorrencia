195e064057e5d75519fce6ac5d0c5e304b2ce296
artur <artur@192.168.0.107>
2021-03-13 17:49:08 -0300

    Atualizando

---
+++ InfraSW-Concorrencia/src/player/MusicPlayer.java
@@ -0,0 +1,10 @@
+package player;
+
+public class MusicPlayer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- InfraSW-Concorrencia/src/player/Playlist.java
+++ InfraSW-Concorrencia/src/player/Playlist.java
@@ -1,10 +1,8 @@
 package player;
 
+import java.util.ArrayList;
+import java.util.concurrent.locks.*;
+
 public class Playlist {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
 
 }
