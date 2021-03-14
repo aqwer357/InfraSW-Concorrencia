f3e1f79a929d730800d068b218782041c0e73ad6
aca7 <aca7@cin.ufpe.br>
2021-03-13 21:35:16 -0300

    Esboço de MusicPlayer

--- InfraSW-Concorrencia/src/player/MusicPlayer.java
+++ InfraSW-Concorrencia/src/player/MusicPlayer.java
@@ -6,9 +6,47 @@ import java.util.Scanner;
 public class MusicPlayer {
 
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		
-		final Playlist playlist;
+		try {
+			Scanner in = new Scanner(System.in);
+			
+			final Playlist playlist = new Playlist();
+			
+			Runnable addRemoveSong = () -> {
+				while (true) {
+					try {
+						String command = in.next();
+						String song = in.next();
+						
+						if (command.equals("add")) {
+							playlist.add();
+						} else {
+							playlist.remove();
+						}
+						
+					} catch (InterruptedException e) {
+						System.out.println("Erro em addRemoveSong()");
+					}
+				}
+				
+			};
+			
+			Runnable showPlaylist = () -> {
+				while (true) {
+					try {
+						playlist.show();
+						
+					} catch (InterruptedException e) {
+						System.out.println("Erro em showPlaylist()");
+					}
+				}
+			};
+	
+			
+			addRemoveSong.run();
+			
+		} catch (Exception ignored) {
+			
+		}
 	}
 
 }
